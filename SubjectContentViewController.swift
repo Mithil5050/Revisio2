2b202b5c56131c82bb623fe8a3e9cf7cf306b650
talwarayaana <talwarayaana@gmail.com>
2025-11-25 16:16:49 +0530

    study view controller and subject view controller

---
+++ SubjectContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubjectContentViewController.swift
+//  Revisio
+//
+//  Created by SDC-USER on 25/11/25.
+//
+
+import UIKit
+
+class SubjectContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
